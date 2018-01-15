326d1f0a8527eef0e81dbd435bdfbc3c0e834e2d
Ben Castan <ben@castanfamily.com>
2018-01-15 16:40:58 +1100

    Added email sub and created a message string from results

--- check_once.py
+++ check_once.py
@@ -1,8 +1,14 @@
 import subprocess
+import email.message
+import email.policy
+import email.utils
+import sys
+
 
 # Value to check for:: Panic indicates immediate attention required
 alert = 90
 warn = 85
+
 
 # If you don't want all partitions reported on set to False
 checkNorms = False
@@ -15,6 +21,13 @@ AWS = ".external.prd.sitesuite.io"
 HOST = ["db-01a" + CLUSTER, "db-01b" + CLUSTER, "backup-01" + CLUSTER, "nfs-01" + CLUSTER, "cpanel" + CLUSTER,
         "mfilter-a-0" + AWS, "mfilter-b-0" + AWS, "mda-a-0" + AWS, "webmail-a-0" + AWS, "webmail-b-0" + AWS]
 COMMAND = "sudo df -h"
+
+
+def print_header():
+    print('------------------------------------')
+    print('     HDD Partition Checker')
+    print('------------------------------------')
+    print()
 
 
 def dedupe(x):
@@ -57,15 +70,49 @@ def check_once():
                             print("Host: {}".format(thisHost))
                             print("Partition {} is at {}%".format(str(splitline[5]), str(splitline[4][:-1])))
                             print()
+    return urgents, warns, norms
 
-    print('Urgents:')
-    print(dedupe(urgents))
-    print('Warns:')
-    print(dedupe(warns))
-    if checkNorms:
-        print("NORMS")
-        print(dedupe(norms))
+
+def build_text(urgs, warn, norm):
+    message = "URGENT: \n"
+    message = "{0}{1}".format(message, ('\n'.join(map(str, urgs))))
+    message = "{0}{1}".format(message,("\nWARN:\n"))
+    message = "{0}{1}".format(message, ('\n'.join(map(str, warn))))
+    message = "{0}{1}".format(message, ("\nNORM:\n"))
+    message = "{0}{1}".format(message, ('\n'.join(map(str, norm))))
+
+    return message
+
+
+def send_emails(text):
+
+    # Foundations of Python network Programming, Third Edition
+    # https://github.com/brandon-rhodes/fonp/bvlob/m/py3/chapter12/build_bhasic_email.py
+
+
+    # text = """Hello,
+    # This is a basic message from Chapter 12.
+    # - Anonymous"""
+
+    message = email.message.EmailMessage(email.policy.SMTP)
+    message['To'] = 'recipient@example.com'
+    message['From'] = 'Test Sender <sender@example.com>'
+    message['Subject'] = 'Test Message, Chapter 12'
+    message['Date'] = email.utils.formatdate(localtime=True)
+    message['Message-ID'] = email.utils.make_msgid()
+    message.set_content(text)
+    sys.stdout.buffer.write(message.as_bytes())
 
 
 if __name__ == "__main__":
-    check_once()
+    print_header()
+    results = check_once()
+    #print('Urgents:')
+    urgents = (dedupe(urgents))
+    #print('Warns:')
+    warns = (dedupe(warns))
+    if checkNorms:
+        #print("NORMS")
+        norms = (dedupe(norms))
+    text = build_text(urgents, warns, norms)
+    send_emails(text)
